b758ed5d6914d294bc79bc6e7b077f2c2e6eed9b
Rama <86864383+ramamdeeCode@users.noreply.github.com>
2022-11-16 18:37:47 -0500

    create function to list all reservation

--- back-end/src/reservations/reservations.controller.js
+++ back-end/src/reservations/reservations.controller.js
@@ -1,12 +1,18 @@
+const service = require("./reservations.service");
+const asyncErrorBoundary = require("../errors/asyncErrorBoundary");
+
 /**
  * List handler for reservation resources
  */
 async function list(req, res) {
-  res.json({
-    data: [],
-  });
+  const { date, mobileNumber } = res.locals;
+  const data = mobileNumber
+    ? await service.searchList(mobileNumber)
+    : await service.list(date);
+
+  res.json({ data });
 }
 
 module.exports = {
-  list,
+  list: [asyncErrorBoundary(list)],
 };
