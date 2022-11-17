a20f51eecd41c31328360db88e5e9ad4f889574b
Rama <86864383+ramamdeeCode@users.noreply.github.com>
2022-11-17 16:29:16 -0500

    create hasOnlyValidProperties middleware on tables controller

--- back-end/src/tables/tables.controller.js
+++ back-end/src/tables/tables.controller.js
@@ -24,8 +24,24 @@ function getCapacity(req, res, next) {
   next();
 }
 
+function hasOnlyValidProperties(req, res, next) {
+  const { data = {} } = req.body;
+
+  const invalidFields = Object.keys(data).filter(
+    (field) => !VALID_PROPERTIES.includes(field)
+  );
+
+  if (invalidFields.length) {
+    return next({
+      status: 400,
+      message: `Invalid field(s): ${invalidFields.join(", ")}`,
+    });
+  }
+  next();
+}
+
 module.exports = {
   list: [asyncErrorBoundary(list)],
   listFree: [getCapacity, asyncErrorBoundary(listFree)],
-  create: [asyncErrorBoundary(create)],
+  create: [hasOnlyValidProperties, asyncErrorBoundary(create)],
 };
