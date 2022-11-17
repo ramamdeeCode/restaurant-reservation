1073c7e05a329e86ba855cf77966bfd7fae0d072
Rama <86864383+ramamdeeCode@users.noreply.github.com>
2022-11-17 14:44:04 -0500

    create listFree tables services

--- back-end/src/tables/tables.service.js
+++ back-end/src/tables/tables.service.js
@@ -4,6 +4,19 @@ async function list() {
   return knex("tables").select("*").orderBy("table_name");
 }
 
+// list tables that are not occupied
+async function listFree(/* minCapacity */) {
+  //filters list of tables by capacity. This functionality breaks testing but I want to implement it for portfolio.
+  return (
+    knex("tables")
+      .select("*")
+      .where({ reservation_id: null })
+      // .andWhere("capacity", ">=", minCapacity)   filters list of tables by capacity. This functionality breaks testing but I want to implement it for portfolio.
+      .orderBy("table_name")
+  );
+}
+
 module.exports = {
   list,
+  listFree,
 };
