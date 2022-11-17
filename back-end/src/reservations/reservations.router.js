db6e3268ed8b98024675528a2821bb7a115ab975
Rama <86864383+ramamdeeCode@users.noreply.github.com>
2022-11-17 10:30:45 -0500

    update reservation router

--- back-end/src/reservations/reservations.router.js
+++ back-end/src/reservations/reservations.router.js
@@ -9,6 +9,17 @@ const controller = require("./reservations.controller");
 const methodNotAllowed = require("../errors/methodNotAllowed");
 
 router
+  .route("/:reservation_id/status")
+  .put(controller.updateStatus)
+  .all(methodNotAllowed);
+
+router
+  .route("/:reservation_id")
+  .get(controller.read)
+  .put(controller.update)
+  .all(methodNotAllowed);
+
+router
   .route("/")
   .get(controller.list)
   .post(controller.create)
