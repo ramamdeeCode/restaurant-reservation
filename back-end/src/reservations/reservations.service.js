05a5d722d9d406ef2415f74048129d70d1c9794c
Rama <86864383+ramamdeeCode@users.noreply.github.com>
2022-11-17 08:22:05 -0500

    create reservation update service

--- back-end/src/reservations/reservations.service.js
+++ back-end/src/reservations/reservations.service.js
@@ -29,9 +29,17 @@ function read(reservation_id) {
   return knex("reservations").select("*").where({ reservation_id }).first();
 }
 
+function update(updatedReservation) {
+  return knex("reservations")
+    .select("*")
+    .where({ reservation_id: updatedReservation.reservation_id })
+    .update(updatedReservation, "*");
+}
+
 module.exports = {
   list,
   searchList,
   create,
   read,
+  update,
 };
