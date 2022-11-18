8591e189f88d9f0abd36b9425044ef433d5f3dbe
Rama <86864383+ramamdeeCode@users.noreply.github.com>
2022-11-18 16:13:27 -0500

    update router

--- front-end/src/layout/Routes.js
+++ front-end/src/layout/Routes.js
@@ -5,6 +5,9 @@ import Dashboard from "../dashboard/Dashboard";
 import NotFound from "./NotFound";
 import { today } from "../utils/date-time";
 import ReservationForm from "../reservations/ReservationForm";
+import SeatForm from "../reservations/SeatForm";
+import TableNew from "../tables/TableNew";
+import Search from "../search/Search";
 
 /**
  * Defines all the routes for the application.
@@ -26,6 +29,22 @@ function Routes() {
 
       <Route exact={true} path="/reservations/new">
         <ReservationForm editMode={false} />
+      </Route>
+
+      <Route exact={true} path="/reservations/:reservation_id/seat">
+        <SeatForm />
+      </Route>
+
+      <Route exact={true} path="/reservations/:reservation_id/edit">
+        <ReservationForm editMode={true} />
+      </Route>
+
+      <Route exact={true} path="/tables/new">
+        <TableNew />
+      </Route>
+
+      <Route exact={true} path="/search">
+        <Search />
       </Route>
 
       <Route path="/dashboard">
