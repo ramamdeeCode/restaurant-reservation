beb073b39621f4bb076822e8c2fd22bcf7a5a7ca
Rama <86864383+ramamdeeCode@users.noreply.github.com>
2022-11-18 14:31:14 -0500

    createReservation in api

--- front-end/src/utils/api.js
+++ front-end/src/utils/api.js
@@ -67,3 +67,24 @@ export async function listReservations(params, signal) {
     .then(formatReservationDate)
     .then(formatReservationTime);
 }
+
+/**
+ * Saves reservation to the database.
+ * @param reservation
+ *  the reservation to save
+ * @param signal
+ *  optional AbortController.signal
+ * @returns {Promise<reservation>}
+ *  a promise that resolves the saved reservation, which will now have an `id` property.
+ */
+
+export async function createReservation(reservation, signal) {
+  const url = new URL(`${API_BASE_URL}/reservations`);
+  const options = {
+    method: "POST",
+    headers,
+    body: JSON.stringify({ data: reservation }),
+    signal,
+  };
+  return await fetchJson(url, options, {});
+}
